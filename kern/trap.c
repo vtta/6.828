afdebfdf2ecc6427d96eedc15a7ab22f959b3650
vtta <vtta0124@gmail.com>
2019-05-07 11:03:42 +0800

    lab3 ex7: syscall

--- kern/trap.c
+++ kern/trap.c
@@ -157,6 +157,15 @@ trap_dispatch(struct Trapframe *tf)
 	case T_BRKPT:
 		monitor(tf);
 		return;
+	case T_SYSCALL:
+		tf->tf_regs.reg_eax = syscall(
+			tf->tf_regs.reg_eax,
+			tf->tf_regs.reg_edx,
+			tf->tf_regs.reg_ecx,
+			tf->tf_regs.reg_ebx,
+			tf->tf_regs.reg_edi,
+			tf->tf_regs.reg_esi);
+		return;
 	default:
 		break;
 	}
